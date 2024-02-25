e286ee2fbaa4864009c13c41da20874f6188b5d8
znyar <znyaroslav845@gmail.com>
2024-02-25 17:28:39 +0300

    init

---
+++ src/main/java/com/znyar/simplepaysystem/SimplePaySystemApplication.java
@@ -0,0 +1,13 @@
+package com.znyar.simplepaysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePaySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePaySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/znyar/simplepaysystem/SimplePaySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znyar.simplepaysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
